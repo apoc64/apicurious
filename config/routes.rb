463113884f173238c79a41f2e698f55eeaedd730
apoc64 <sschwedt@gmail.com>
2018-07-03 11:44:18 -0600

    user can logout with navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  delete 'signout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:show]
 end
