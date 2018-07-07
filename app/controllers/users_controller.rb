6644b9bd4975be21bf74a7fdad9d70d794971d1e
apoc64 <sschwedt@gmail.com>
2018-07-07 17:54:11 -0600

    styles home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# require_relative '../services/github'
-
 class UsersController < ApplicationController
   def show
     @presenter = ProfilePresenter.new(params[:id], current_user)
--- app/models/repo_collection.rb
+++ app/models/repo_collection.rb
@@ -2,12 +2,10 @@ class RepoCollection
   attr_reader :repositories
 
   def initialize(attributes)
-    # binding.pry
     @repositories = create_repositories(attributes)
   end
 
   def create_repositories(attributes)
-    # binding.pry
     attributes.map do |repo_attributes|
       Repository.new(repo_attributes)
     end
